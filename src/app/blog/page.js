b47dd4f675370ff933ec680b93df388c47e1234c
primeK31 <ashirbekov005@gmail.com>
2024-06-05 21:54:27 +0500

    maybe fix2

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -17,8 +17,8 @@ const BlogPage = async () => {
         <Heads>
         </Heads>
           {data.map((blog, index) => (
-            <a href={`/blog/${blog.id}`}>
-              <div key={index} className="bg-black rounded-lg dark:bg-gray-800 w-full md:w-auto m-10 p-10">
+            <a key={index} href={`/blog/${blog.id}`}>
+              <div className="bg-black rounded-lg dark:bg-gray-800 w-full md:w-auto m-10 p-10">
                   <h1 className="text-2xl">{blog.title}</h1>
                   <p className="mt-5">{blog.description}</p>
 
